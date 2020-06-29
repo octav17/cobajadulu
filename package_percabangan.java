c776609055c8bf808c9c058134b82c9f8ee78411
Octaviany <63240238+octav17@users.noreply.github.com>
2020-06-29 13:42:41 +0700

    delete comment 1

--- package_percabangan.java
+++ package_percabangan.java
@@ -41,10 +41,3 @@ public class Percabangan {
         txt_hasil.setText(bilangan);
     }
 }
-/* @param  x the first {@code int} to compare
- * @param  y the second {@code int} to compare
- * @return the value {@code 0} if {@code x == y};
- *         a value less than {@code 0} if {@code x < y}; and
- *         a value greater than {@code 0} if {@code x > y}
- * @since 1.7
- */
