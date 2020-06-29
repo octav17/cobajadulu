63b3866c8fd4a4e654afaec807a4dc6a367521a3
Octaviany <63240238+octav17@users.noreply.github.com>
2020-06-29 13:46:40 +0700

    delete ws n c ke 2

--- package_percabangan.java
+++ package_percabangan.java
@@ -2,23 +2,19 @@ package percabangan;
 public class Percabangan {
     public static void main(String[] args) {
         int x=3;
-        //buat switch case
         switch (x){
-                
             case 1:
                 System.out.println("nilai x = 1");
                 break;
-                
             case 2:
                 System.out.println("nilai x = 2");
                 break;
-                
             default:
                 System.out.println("nilai a tidak di katahui");
     }
 }
     private void bt_hasilActionPerformed(java.awt.event.ActionEvent evt) {                                         
-        // TODO add your handling code here:
+
         switch(pilih){
             case 1:
             bil2 = Double.parseDouble(String.valueOf(txt_hasil.getText()));
@@ -31,7 +27,6 @@ public class Percabangan {
             bilangan = Double.toString(jumlah);            
             break;
         
-        //perlu menambah 1 case lagi
         case 4:
             bil2 = Double.parseDouble(String.valueOf(txt_hasil.getText()));
             jumlah = bil1 / bil2;
