628be0c5ccf63f91ce51b975d6e7273f8749b415
Im Yoona <47500322+yoonaim@users.noreply.github.com>
2020-06-29 13:41:24 +0700

    add and delete web3

--- package_percabangan.java
+++ package_percabangan.java
@@ -13,9 +13,6 @@ public class Percabangan {
                 System.out.println("nilai x = 2");
                 break;
                 
-            case 3:
-                System.out.println("nilai x = 3");
-                break;
             default:
                 System.out.println("nilai a tidak di katahui");
     }
@@ -33,11 +30,7 @@ public class Percabangan {
             jumlah = bil1 - bil2;
             bilangan = Double.toString(jumlah);            
             break;
-        case 3:
-            bil2 = Double.parseDouble(String.valueOf(txt_hasil.getText()));
-            jumlah = bil1 * bil2;
-            bilangan = Double.toString(jumlah);            
-            break;
+        
         //perlu menambah 1 case lagi
         case 4:
             bil2 = Double.parseDouble(String.valueOf(txt_hasil.getText()));
@@ -45,9 +38,13 @@ public class Percabangan {
             bilangan = Double.toString(jumlah);            
             break;  
         }
+        txt_hasil.setText(bilangan);
     }
 }
 /* @param  x the first {@code int} to compare
  * @param  y the second {@code int} to compare
  * @return the value {@code 0} if {@code x == y};
  *         a value less than {@code 0} if {@code x < y}; and
+ *         a value greater than {@code 0} if {@code x > y}
+ * @since 1.7
+ */
